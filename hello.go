fa3846fdd91cf39156309ce386647f0a1c28ca1f
Ronzin Vladimir Igorevich <ronzin9696@gmail.com>
2023-04-18 17:30:20 +0500

    hello: initial commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+	//"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+)
+
+func Hello() string {
+	return quoteV3.HelloV3()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
+}
